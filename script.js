9cf083c1e38a94a6de1249ec1c829fb94954ed30
Jonathan <jonathancamiling@gmail.com>
2021-09-05 23:17:32 -0700

    completed script file

---
+++ script.js
@@ -0,0 +1,32 @@
+// function to remove show-active class and add to clicked 
+function setNewActive(el){
+  //select all content-body
+  let contentBodies = document.getElementsByClassName('content-body')
+  //remove show-active 
+  for (contentBody of contentBodies){
+    contentBody.classList.remove('show-active')
+  }
+  // add show-active back to clicked 
+  document.getElementById(el.textContent.trim()).classList.add('show-active')
+  
+  let tabs = document.getElementsByClassName('tab');
+  for (let tab of tabs){
+    tab.classList.remove('tab-active')
+  }
+  el.classList.add('tab-active')
+}
+  
+  
+  
+
+// select tab class, and loop through +
+
+const tabs = document.getElementsByClassName('tab');
+for (let tab of tabs){
+// add click listener to each tab
+tab.addEventListener( 'click', function(e) {
+  setNewActive(e.currentTarget);
+})
+}
+
+ 
